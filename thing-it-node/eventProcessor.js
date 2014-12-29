0262ec8dba6a37909ca8290c9ddcd143b28d5656
Gille, Marc <Marc.Gille@sungard.com>
2014-12-29 14:34:25 +0200

    More Event Processor fixes.

--- thing-it-node/eventProcessor.js
+++ thing-it-node/eventProcessor.js
@@ -19,25 +19,30 @@ function EventProcessor() {
 	/**
 	 * 
 	 */
-	EventProcessor.prototype.register = function(callback) {
-		this.series = [];
-		this.callback = callback;
-
+	EventProcessor.prototype.register = function() {
 		for (var n = 0; n < this.observables.length; ++n) {
 			var path = this.observables[n].split(".");
 
+			if (!this[path[0]]) {
+				this[path[0]] = {};
+			}
+
+			this[path[0]][path[1]] = {
+				series : []
+			};
+
 			this.node[path[0]][path[1]].eventProcessors.push(this);
 		}
+
+		this.checkMatch = new Function("input", "return " + this.match);
 	};
 
 	/**
 	 * Records Sensor data for evaluation at the end of the window
 	 */
-	EventProcessor.prototype.push = function(sensor, data) {
-		console.log("Pushing data " + data);
-
-		this.series.push({
-			data : data,
+	EventProcessor.prototype.push = function(sensor, value) {
+		this[sensor.device.id][sensor.id].series.push({
+			value : value,
 			timestamp : new Date().getTime()
 		});
 	};
@@ -66,21 +71,39 @@ function EventProcessor() {
 			var self = this;
 
 			setInterval(function() {
-				console.log("Evaluating interval");
-				console.log(self.series);
+				console.log("Interval for " + self.id);
 
-				// if (this.series.length >= this.length) {
-				// console.log("===> Callback");
-				//
-				// // Should be events and series for all sensors
-				//
-				// this.callback(this.series, this.event);
-				// }
+				if (self.checkMatch()) {
+					console.log("Match");
 
-				self.series = [];
+					self.execute();
+				}
+
+				// Reinitialize series
+
+				for (var n = 0; n < self.observables.length; ++n) {
+					var path = self.observables[n].split(".");
+
+					self[path[0]][path[1]] = {
+						series : []
+					};
+				}
 			}, this.window.duration)
 		}
 	};
+
+	/**
+	 * 
+	 */
+	EventProcessor.prototype.minimum = function(series) {
+		var minimum = 10000000000; // TODO
+
+		for (var n = 0; n < series.length; ++n) {
+			minimum = Math.min(minimum, series[n].value);
+		}
+
+		return minimum;
+	}
 
 	/**
 	 * Execute the Event Processor logic.
