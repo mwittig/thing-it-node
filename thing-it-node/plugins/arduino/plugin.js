bbb8d01d6a373406dcd5bc10aed5fe5dd634a882
Gille, Marc <Marc.Gille@sungard.com>
2015-01-16 13:05:57 +0200

    Enhancements on configuration simplicity.

--- thing-it-node/plugins/arduino/plugin.js
+++ thing-it-node/plugins/arduino/plugin.js
@@ -25,17 +25,17 @@ function Arduino() {
 				id : "2",
 				label : "2"
 			}, {
-				id : "~3",
-				label : "~3"
+				id : "3",
+				label : "3"
 			}, {
 				id : "4",
 				label : "4"
 			}, {
-				id : "~5",
-				label : "~5"
+				id : "5",
+				label : "5"
 			}, {
-				id : "~6",
-				label : "~6"
+				id : "6",
+				label : "6"
 			}, {
 				id : "7",
 				label : "7"
@@ -43,14 +43,14 @@ function Arduino() {
 				id : "8",
 				label : "8"
 			}, {
-				id : "~9",
-				label : "~9"
+				id : "9",
+				label : "9"
 			}, {
-				id : "~10",
-				label : "~10"
+				id : "10",
+				label : "10"
 			}, {
-				id : "~11",
-				label : "~11"
+				id : "11",
+				label : "11"
 			}, {
 				id : "12",
 				label : "12"
