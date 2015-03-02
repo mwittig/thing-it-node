d06fa42c10eea10f38483f44bb9a48fe0823f580
Gille, Marc <Marc.Gille@sungard.com>
2015-03-01 19:50:18 -0500

    Pairing Agent functionality started.

--- examples/apple-device-dacp/configuration.js
+++ examples/apple-device-dacp/configuration.js
@@ -2,20 +2,24 @@ module.exports = {
 	label : "Home",
 	id : "home",
 	devices : [ {
-		label : "Apple TV",
-		id : "appleTv",
+		label : "Apple Device Manager",
+		id : "appleDeviceManager",
 		plugin : "apple-device/appleDevice",
 		actors : [ {
 			label : "Apple TV",
 			id : "appleTv",
 			type : "dacpActor",
+			configuration : {
+				host : "127.0.0.1",
+				port : 3689,
+				pairingCode : ""
+			}
+		}, {
+			label : "Pairing Agent",
+			id : "pairingAgent",
+			type : "pairingAgent"
 		} ],
-		sensors : [],
-		configuration : {
-			host : "127.0.0.1",
-			port : 3689,
-			pairingCode : ""
-		}
+		sensors : []
 	} ],
 	services : [],
 	eventProcessors : []
