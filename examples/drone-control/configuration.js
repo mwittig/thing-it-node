8de2ad8d5c8da5ca211c60578e91d63f1e73ef00
Gille, Marc <Marc.Gille@sungard.com>
2015-03-10 20:21:23 -0400

    Added first work on AR Drones and also introduced "tangible devices" with their own services.

---
+++ examples/drone-control/configuration.js
@@ -0,0 +1,14 @@
+module.exports = {
+	label : "Home",
+	id : "home",
+	devices : [ {
+		label : "Parrot Helicopter Drone",
+		id : "parrotHelicopterDrone",
+		plugin : "ar-drone/ardrone",
+        configuration : [ ],
+		actors : [ ],
+		sensors : [ ]
+	} ],
+	services : [],
+	eventProcessors : []
+};
