965f65c6088b3dbf01a16150eb3274add73b5e06
Gille, Marc <Marc.Gille@sungard.com>
2015-03-11 12:07:50 -0400

    Code structure for IR Remote.

--- options.js
+++ options.js
@@ -1,11 +1,11 @@
 module.exports = {
-	port : 3001,
-	protocol : "http",
-	nodeConfigurationFile : "../examples/drone-control/configuration.js",
-	dataDirectory : "../thing-it-data",
-	simulated : false,
-	hotDeployment : false,
-	verifyCallSignature : false,
-	publicKeyFile : "",
-	signingAlgorithm : "RSA-SHA256"
+    port: 3001,
+    protocol: "http",
+    nodeConfigurationFile: "../examples/universal-remote-control/configuration.js",
+    dataDirectory: "../thing-it-data",
+    simulated: true,
+    hotDeployment: false,
+    verifyCallSignature: false,
+    publicKeyFile: "",
+    signingAlgorithm: "RSA-SHA256"
 };
