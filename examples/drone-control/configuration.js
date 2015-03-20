5094dfbc0ad2ce93b521efc3d5de1d69f3eafa4f
Gille, Marc <Marc.Gille@sungard.com>
2015-03-20 06:23:25 -0400

    More Drone fixes/enhancements.

--- examples/drone-control/configuration.js
+++ examples/drone-control/configuration.js
@@ -1,14 +1,21 @@
 module.exports = {
-	label : "Home",
-	id : "home",
-	devices : [ {
-		label : "Parrot Rolling Spider Drone",
-		id : "parrotRollingSpiderDrone",
-		plugin : "ar-drone/ardroneBluetoothLE",
-        configuration : [ ],
-		actors : [ ],
-		sensors : [ ]
-	} ],
-	services : [],
-	eventProcessors : []
+    label: "Home",
+    id: "home",
+    devices: [{
+        label: "Parrot Rolling Spider Drone",
+        id: "parrotRollingSpiderDrone",
+        plugin: "ar-drone/ardroneBluetoothLE",
+        configuration: [],
+        actors: [],
+        sensors: []
+    }, {
+        label: "Parrot AR Drone",
+        id: "parrotARDrone",
+        plugin: "ar-drone/ardroneWifi",
+        configuration: [],
+        actors: [],
+        sensors: []
+    }],
+    services: [],
+    eventProcessors: []
 };
