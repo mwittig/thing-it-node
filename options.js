327aed71356a7704089ec3d66800e99169982188
Gille, Marc <Marc.Gille@sungard.com>
2015-03-26 18:31:55 -0400

    More on reverse proxy.

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: 3001,
     protocol: "http",
-    nodeConfigurationFile: "../examples/camera-and-microphone/configuration.js",
+    nodeConfigurationFile: "../examples/simple-lighting/configuration.js",
     dataDirectory: "../thing-it-data",
     simulated: true,
     hotDeployment: false,
