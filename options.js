68c29d19f2ac232ecde61ef8afd39ad9e8dcb889
Gille, Marc <Marc.Gille@sungard.com>
2015-06-29 16:01:52 -0400

    Cleanup for proxy mode.

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     port: 3001,
     protocol: "http",
     nodeConfigurationFile: "../examples/our-things/configuration.js",
@@ -11,13 +11,12 @@ module.exports = {
     signingAlgorithm: "RSA-SHA256",
     authentication: "none",//"user",
     logLevel: "debug"
-};
+};*/
 
-/*
 module.exports = {
     port: 3001,
     protocol: "http",
     uuid: "1c178e30-1cf1-11e5-bbd4-55db91e41791",
     proxy: "http://localhost:3000",
     logLevel: "debug"
-};*/
+};
