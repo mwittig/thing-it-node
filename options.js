06aad7afb8b0e1822bad77ba79bb040d71927e06
Gille, Marc <Marc.Gille@sungard.com>
2015-06-29 15:36:05 -0400

    Cleanup for proxy mode.

--- options.js
+++ options.js
@@ -1,18 +1,23 @@
 module.exports = {
-    logLevel: "debug",
     port: 3001,
     protocol: "http",
     nodeConfigurationFile: "../examples/our-things/configuration.js",
     dataDirectory: "../thing-it-data",
     usersDirectory: "../thing-it-users",
     simulated: true,
-    hotDeployment: false,
-    proxy: {
-        mode: "local"/*,
-        reverseProxy: "http://localhost:3000"*/
-    },
+    proxy: "local",
     verifyCallSignature: false,
     publicKeyFile: "",
     signingAlgorithm: "RSA-SHA256",
-    authentication: {mode: "user"}
+    authentication: "none",//"user",
+    logLevel: "debug"
 };
+
+/*
+module.exports = {
+    port: 3001,
+    protocol: "http",
+    uuid: "1c178e30-1cf1-11e5-bbd4-55db91e41791",
+    proxy: "http://localhost:3000",
+    logLevel: "debug"
+};*/
