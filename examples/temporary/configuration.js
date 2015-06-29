847f996c439429ece98318a944871cc1b43cac15
Gille, Marc <Marc.Gille@sungard.com>
2015-06-29 17:15:24 -0400

    Cleanup for proxy mode.

---
+++ examples/temporary/configuration.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "dataTypes": {},
+    "simulated": true,
+    "id": "node1",
+    "label": "Node 1",
+    "uuid": "84f2f730-1dc0-11e5-9850-c56b9b917679",
+    "devices": [{
+        "actors": [{"type": "led", "id": "led1", "label": "LED 1", "configuration": {"pin": "12"}}],
+        "sensors": [{
+            "type": "button",
+            "id": "button1",
+            "label": "Button 1",
+            "configuration": {
+                "pin": "12",
+                "holdtime": 500,
+                "sendClickEvents": true,
+                "sendDownEvents": true,
+                "sendHoldEvents": true
+            }
+        }],
+        "services": [],
+        "plugin": "microcontroller/arduino",
+        "id": "arduino1",
+        "label": "Arduino Uno 1"
+    }],
+    "services": [{
+        "id": "service1",
+        "label": "Service1",
+        "content": {"type": "script", "script": "arduino1.led1.on();"}
+    }],
+    "eventProcessors": [{
+        "id": "eventProcessor1",
+        "label": "Event Processor 1",
+        "content": {"type": "actorService", "actor": "arduino1.led1", "service": "on"},
+        "observables": ["arduino1.button1"],
+        "match": "arduino1.button1.event.type == \"hold\""
+    }],
+    "groups": [],
+    "connectionMode": "simulated",
+    "jobs": [],
+    "data": [],
+    "lastModificationTimestamp": 1435611167939
+};
