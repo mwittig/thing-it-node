67dcd15b1f5464041d6db2768e4192e051997487
Gille, Marc <Marc.Gille@sungard.com>
2015-07-04 13:57:51 -0400

    AirCable SmartDimmer added via module.

--- examples/aircable/configuration.js
+++ examples/aircable/configuration.js
@@ -11,6 +11,22 @@ module.exports = {
         },
         actors: [],
         sensors: []
+    }, {
+        uuid: "9ef7f55f18d448e4888f34ca397753ef",
+        id: "sensorTagLounge",
+        label: "Sensor Tag Lounge",
+        plugin: "ti-sensor-tag/tiSensorTag",
+        configuration: {
+            barometricPressureEnabled: false,
+            irTemperatureEnabled: false,
+            ambientTemperatureEnabled: true,
+            accelerometerEnabled: false,
+            gyroscopeEnabled: false,
+            magnetometerEnabled: false,
+            humidityEnabled: true
+        },
+        actors: [],
+        sensors: []
     }],
     services: [],
     eventProcessors: []
