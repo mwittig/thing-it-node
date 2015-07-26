b879787f4a6c9c525ba8d76963f3cdc22df1499c
Gille, Marc <Marc.Gille@sungard.com>
2015-07-26 10:07:35 -0400

    Event processing of multidimensional device states.

--- examples/aircable/configuration.js
+++ examples/aircable/configuration.js
@@ -15,7 +15,7 @@ module.exports = {
         uuid: "9ef7f55f18d448e4888f34ca397753ef",
         id: "sensorTagLounge",
         label: "Sensor Tag Lounge",
-        plugin: "ti-sensor-tag/tiSensorTag",
+        plugin: "ti-sensortag/tiSensorTag",
         configuration: {
             barometricPressureEnabled: false,
             irTemperatureEnabled: false,
