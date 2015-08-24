6ad6a1d6e731bc61efd1cd391c3ee80c87c33095
Gille, Marc <Marc.Gille@sungard.com>
2015-08-24 08:38:37 +0200

    ITach add-on.

--- examples/itach-ir-remote/configuration.js
+++ examples/itach-ir-remote/configuration.js
@@ -2,11 +2,34 @@ module.exports = {
     label: "Home",
     id: "home",
     devices: [{
-        label: "TV Remote",
-        id: "tv Remote",
+        logLevel: "debug",
+        label: "TV",
+        id: "tv",
         plugin: "itach/tvRemote",
         configuration: {
-            simulated: false,
+            simulated: true,
+            host: "192.168.10.1"
+        },
+        actors: [],
+        sensors: []
+    }, {
+        logLevel: "debug",
+        label: "iRobot Roomba",
+        id: "iRobotRoomba",
+        plugin: "itach/iRobotRoombaRemote",
+        configuration: {
+            simulated: true,
+            host: "192.168.10.1"
+        },
+        actors: [],
+        sensors: []
+    }, {
+        logLevel: "debug",
+        label: "IR Sniffer",
+        id: "irSniffer",
+        plugin: "itach/irSniffer",
+        configuration: {
+            simulated: true,
             host: "192.168.10.1"
         },
         actors: [],
