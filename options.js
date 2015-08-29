cf335a0a21c71c5b61757ef28adaf7342c676e91
Gille, Marc <Marc.Gille@sungard.com>
2015-08-29 10:36:05 +0200

    itach updated.

--- options.js
+++ options.js
@@ -1,19 +1,19 @@
-/*module.exports = {
- port: 3001,
- protocol: "http",
- nodeConfigurationFile: "../examples/our-things/configuration.js",
- dataDirectory: "../thing-it-data",
- usersDirectory: "../thing-it-users",
- simulated: true,
- verifyCallSignature: false,
- authentication: "none",//"user",
- logLevel: "debug"
- };*/
-
 module.exports = {
+    port: 3001,
+    protocol: "http",
+    nodeConfigurationFile: "../examples/itach-ir-remote/configuration.js",
+    dataDirectory: "../thing-it-data",
+    usersDirectory: "../thing-it-users",
+    simulated: true,
+    verifyCallSignature: false,
+    authentication: "none",//"user",
+    logLevel: "debug"
+};
+
+/*module.exports = {
     //uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
     uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
     //proxy: "http://www.thing-it.com",
     proxy: "http://localhost:3000",
     nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/temporary/configuration.js",
-};
+};*/
