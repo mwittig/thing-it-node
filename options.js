8caa2d92a85591310b4502ebbfefa429bee00429
Gille, Marc <Marc.Gille@sungard.com>
2015-08-30 10:57:51 +0200

    Request UUID for Reverse Proxy.

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     port: 3001,
     protocol: "http",
     nodeConfigurationFile: "../examples/itach-ir-remote/configuration.js",
@@ -8,12 +8,12 @@ module.exports = {
     verifyCallSignature: false,
     authentication: "none",//"user",
     logLevel: "debug"
-};
+};*/
 
-/*module.exports = {
+module.exports = {
     //uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
     uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
     //proxy: "http://www.thing-it.com",
     proxy: "http://localhost:3000",
     nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/temporary/configuration.js",
-};*/
+};
