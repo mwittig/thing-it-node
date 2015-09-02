71b1228416e3523f84e06361abdd0e0610b3f932
Gille, Marc <Marc.Gille@sungard.com>
2015-09-02 08:49:16 +0200

    itach updated.

--- options.js
+++ options.js
@@ -11,6 +11,7 @@
 };*/
 
 module.exports = {
+    logLevel: "debug",
     //uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
     uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
     //proxy: "http://www.thing-it.com",
