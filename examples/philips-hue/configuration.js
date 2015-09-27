9e0a8c177cdbda21b5f0c7268e4f953d4cbfafd2
marcgille <latakia42>
2015-09-27 15:48:42 -0400

    Logging for auto-discovery.

--- examples/philips-hue/configuration.js
+++ examples/philips-hue/configuration.js
@@ -2,42 +2,37 @@ module.exports = {
     label: "Suite 1302 Lighting",
     id: "suite1302Lighting",
     autoDiscoveryDeviceTypes: [{
-     plugin: "philips-hue/hueBridge",
-     confirmRegistration: true,
-     persistRegistration: false,
-     defaultConfiguration: {
-     },
-     options: {}
-     }],
+        plugin: "philips-hue/hueBridge",
+        confirmRegistration: true,
+        persistRegistration: false,
+        defaultConfiguration: {},
+        options: {}
+    }],
     devices: [{
         label: "Philips Hue Bridge",
         id: "philipsHueBridge",
         plugin: "philips-hue/hueBridge",
         logLevel: "debug",
-        configuration: {host: "192.168.1.4", userName: "2752a546106c35443652a19216b6953f"},
-        actors: [/*{
-            id: "lightBulbKitchenCounter",
-            label: "Light Bulb Kitchen Counter",
-            type: "lightBulb",
-            configuration: {
-                id: 1
-            }
-        }, {
-            id: "lightBulbCouch",
-            label: "Light Bulb Couch",
+        configuration: {host: "192.168.1.4", userName: "6a61bcc3553ea0f220fc7a4196781a7"},
+        actors: [{
+            id: "lightBulbBedroom",
+            label: "Light Bulb Bedroom",
             type: "lightBulb",
             configuration: {
                 id: 2
-            }
-        }*/], sensors: []
+            },
+            logLevel: "debug"
+        }, {
+            id: "livingColorLampBar",
+            label: "Living Color Lamp Bar",
+            type: "livingColorLamp",
+            configuration: {
+                id: 3
+            },
+            logLevel: "debug"
+        }], sensors: []
     }],
-    groups: [{
-        id: "group1",
-        label: "Lounge",
-        icon: "icon sl-house-1",
-        devices: [],
-        actors: [/*"philipsHueBridge.lightBulbCouch"*/]
-    }],
+    groups: [],
     services: [{
         id: "blackout",
         label: "Blackout",
