e59ec819a3ff1efc424e2f587b9a1765afb8e636
marcgille <latakia42>
2015-10-03 08:45:17 -0400

    Some dependencies resolved.

--- options.js
+++ options.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: 3001,
     protocol: "http",
-    nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/simple-lighting/configuration.js",
-    //nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/temporary/configuration.js",
+    //nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/simple-lighting/configuration.js",
+    nodeConfigurationFile: "/Users/marcgille/git/thing-it-node/examples/temporary/configuration.js",
     dataDirectory: "../thing-it-data",
     usersDirectory: "../thing-it-users",
     simulated: true,
@@ -11,6 +11,6 @@ module.exports = {
     logLevel: "debug",
     uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
     //uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
-    //proxy: "http://www.thing-it.com",
+    proxy: "http://www.thing-it.com",
     //proxy: "http://localhost:3000",
 };
