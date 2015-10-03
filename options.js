cdcbb59704d9d8ec72946fcfee3c53fc43d895e7
marcgille <latakia42>
2015-10-03 18:03:39 -0400

    Bootstrap logic extended.

--- options.js
+++ options.js
@@ -9,8 +9,8 @@ module.exports = {
     verifyCallSignature: false,
     authentication: "none",//"user",
     logLevel: "debug",
-    uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
-    //uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
-    proxy: "http://www.thing-it.com",
-    //proxy: "http://localhost:3000",
+    //uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
+    uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
+    //proxy: "http://www.thing-it.com",
+    proxy: "http://localhost:3000",
 };
