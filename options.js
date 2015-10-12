db9f6affe054eb1ab3894be993a04ddc399c9676
marcgille <latakia42>
2015-10-12 19:24:03 +0200

    HTTPS in sample options file.

--- options.js
+++ options.js
@@ -10,8 +10,8 @@ module.exports = {
     verifyCallSignature: false,
     authentication: "none",//"user",
     logLevel: "debug",
-    //uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
+    uuid: "06f66250-241d-11e5-908b-d1730e52aab0",
     //uuid: "c0fb5520-2418-11e5-948f-2764e628de85",
-    //proxy: "https://www.thing-it.com",
+    proxy: "https://www.thing-it.com",
     //proxy: "http://localhost:3000",
 };
