3da7ba84fe8cbc8dde9210aeb40539a857f9d090
marcgille <latakia42>
2016-01-07 16:23:45 -0500

    temperature-alert Plugin added.

--- options.js
+++ options.js
@@ -4,7 +4,7 @@ module.exports = {
     nodeConfigurationsDirectory: "/Users/marcgille/git/thing-it-node/configurations",
     dataDirectory: "../thing-it-data",
     usersDirectory: "../thing-it-users",
-    simulated: true,
+    simulated: false,
     verifyCallSignature: false,
     authentication: "none",//"user",
     logLevel: "debug",
