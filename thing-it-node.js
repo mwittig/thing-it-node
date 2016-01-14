5cc8f3d1f58eedfa5c64500e39df73688b26866b
marcgille <latakia42>
2016-01-13 20:54:28 -0500

    Div. on config management.

--- thing-it-node.js
+++ thing-it-node.js
@@ -1,5 +1,6 @@
 // Imports
 
+var package = require('./package.json');
 var options = require("./options");
 var node = require("./lib/node");
 var utils = require("./lib/utils");
@@ -35,49 +36,40 @@ app.use(bodyParser.json());
 
 var server = app
     .listen(
-    options.port,
-    function () {
-        console.log("\n");
-        console
-            .log("---------------------------------------------------------------------------");
-        console.log(" [thing-it-node] at http://%s:%s", server.address().address, server.address().port);
-        console.log("\n");
-
-        if (options.proxy == "local") {
-            console.log(" Node Configuration File: %s",
-                options.nodeConfigurationFile);
+        options.port,
+        function () {
+            console.log("\n");
+            console
+                .log("---------------------------------------------------------------------------");
+            console.log(" [thing-it-node] at http://%s:%s", server.address().address, server.address().port);
+            console.log("\n");
+            console.log(" Firmware Version       : %s",
+                package.version);
             console.log(" Simulated              : %s",
                 options.simulated);
-            console.log(" Hot Deployment         : %s",
-                options.hotDeployment);
-            console.log(" Verify Call Signature  : %s",
-                options.verifyCallSignature);
-            console.log(" Public Key File        : %s",
-                options.publicKeyFile);
-            console.log(" Signing Algorithm      : %s",
-                options.signingAlgorithm);
-        }
-        else {
-            console.log(" UUID                   : %s",
-                options.uuid);
-        }
+            console.log(" Node Configuration File: %s",
+                options.nodeConfigurationFile);
+            if (options.proxy != "local") {
+                console.log(" UUID                   : %s",
+                    options.uuid);
+            }
 
-        console.log(" Log Level              : %s",
-            options.logLevel);
-        console.log("\n");
-        console
-            .log(" Copyright (c) 2014-2015 Marc Gille. All rights reserved.");
-        console
-            .log("-----------------------------------------------------------------------------");
-        console.log("\n");
+            console.log(" Log Level              : %s",
+                options.logLevel);
+            console.log("\n");
+            console
+                .log(" Copyright (c) 2014-2015 Marc Gille. All rights reserved.");
+            console
+                .log("-----------------------------------------------------------------------------");
+            console.log("\n");
 
-        try {
-            io.listen(server);
+            try {
+                io.listen(server);
 
-            node.bootstrap(options, app, io);
-        } catch (x) {
-            console.error("Cannot start node: ", x);
+                node.bootstrap(options, app, io);
+            } catch (x) {
+                console.error("Cannot start node: ", x);
 
-            process.exit();
-        }
-    });
+                process.exit();
+            }
+        });
