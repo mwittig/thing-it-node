977906770dae3931a35c16d67d78700a43211458
marcgille <latakia42>
2016-01-23 10:57:58 -0500

    Firmware update.

--- options.js
+++ options.js
@@ -10,8 +10,8 @@ module.exports = {
     logLevel: "debug",
     //proxy: "https://www.thing-it.com",
     //uuid: "fe52f150-745c-11e5-a37a-a3e2989e70a5",
-    //proxy: "http://localhost:3003",
-    //uuid: "c0fb5520-2418-11e5-948f-2764e628de85"
     proxy: "http://localhost:3003",
-    uuid: "30f38660-becc-11e5-a877-73fa028e4e6d"
+    uuid: "c0fb5520-2418-11e5-948f-2764e628de85"
+    //proxy: "http://localhost:3003",
+    //uuid: "30f38660-becc-11e5-a877-73fa028e4e6d"
 };
