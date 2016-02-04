bdceb5079c39a836c37e146aea6303ca2f16a7d1
marcgille <latakia42>
2016-02-03 20:55:00 -0500

    Some logging.

--- options.js
+++ options.js
@@ -8,8 +8,8 @@ module.exports = {
     verifyCallSignature: false,
     authentication: "none",//"user",
     logLevel: "debug",
-    //proxy: "https://www.thing-it.com",
-    //uuid: "fe52f150-745c-11e5-a37a-a3e2989e70a5",
+    proxy: "https://www.thing-it.com",
+    uuid: "fe52f150-745c-11e5-a37a-a3e2989e70a5",
     //proxy: "http://localhost:3003",
     //uuid: "c0fb5520-2418-11e5-948f-2764e628de85"
     //proxy: "http://localhost:3003",
