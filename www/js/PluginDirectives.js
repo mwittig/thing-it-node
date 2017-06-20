4cc79cb90e00d7df6c3a7ed6993a53e801e8f23e
marcgille <latakia42>
2017-06-20 08:54:30 +0200

    Make pairing more robust.

--- www/js/PluginDirectives.js
+++ www/js/PluginDirectives.js
@@ -1,8 +1,19 @@
+var Utils = require('../utils/Utils');
+
 angular.module("ThingItMobile.PluginDirectives", [])
     .filter('unsafe', function ($sce) {
         return function (value) {
             return $sce.trustAsHtml(value);
         };
+    })
+    .filter('moment', function () {
+        return function (input, format) {
+            if (!input) {
+                return '-';
+            }
+
+            return moment(input).local().format(format) + ' @ ' + input + ' @ ' + moment(input);
+        }
     })
     .filter('characters', function () {
         return function (input, chars, breakOnWord) {
@@ -34,7 +45,7 @@ angular.module("ThingItMobile.PluginDirectives", [])
             if (input && input.length > chars) {
                 var prefix = input.substring(0, chars / 2);
                 var postfix = input.substring(input.length - chars / 2, input.length);
-                return prefix + '...' + postfix;
+                return prefix + '…' + postfix;
             }
             return input;
         };
@@ -71,6 +82,15 @@ angular.module("ThingItMobile.PluginDirectives", [])
             return timeString;
         }
     })
+    .filter('degrees', function () {
+        return function (value) {
+            if (value) {
+                return [0 | value, '\u00B0 ', 0 | (value < 0 ? value = -value : value) % 1 * 60, "' ", 0 | value * 60 % 1 * 60, '"'].join('');
+            } else {
+                return "-";
+            }
+        }
+    })
     .filter('yesno', function () {
         return function (value) {
             if (value) {
@@ -80,12 +100,130 @@ angular.module("ThingItMobile.PluginDirectives", [])
             }
         }
     })
-    .directive('ngBlur', function () {
-        return function (scope, elem, attrs) {
-            elem.bind('blur', function () {
-                scope.$apply(attrs.ngBlur);
-            });
-        };
+    .filter('na', function () {
+        return function (value) {
+            if (value) {
+                return value;
+            } else {
+                return "-";
+            }
+        }
+    }).directive('autofocus', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element[0].focus();
+        }
+    };
+}).directive(
+    'tiDateTimeInput',
+    function ($log, $filter) {
+        return {
+            restrict: 'E',
+            require: 'ngModel',
+            scope: {
+                model: "=ngModel",
+                min: "=minDate",
+                blur: "@ngBlur",
+                change: "@ngChange"
+            },
+            template: '<div class="flexLayout flexAlignCenter">' +
+            '<input class="datepicker" style="width: 7em;" type="text">' +
+            '<i class="marginLeft0_2 icon sl-calendar-1"></i>' +
+            '<input class="clockpicker marginLeft0_2" style="width: 6em;" type="text">' +
+            '<i class="marginLeft0_2 icon sl-clock-1"></i></div>',
+            link: function (scope,
+                            element,
+                            attrs) {
+
+                var clockInput = jQuery(element).find('.clockpicker');
+
+                clockInput.prop('readonly', true);
+
+                var clockPicker = clockInput.clockpicker({
+                    placement: 'window',
+                    align: 'left',
+                    donetext: 'Done',
+                    autoclose: true,
+                    twelvehour: true,
+                    beforeShow: function () {
+                        this.value = clockPicker.val();
+                    },
+                    afterDone: function () {
+                        var pickerTime = moment(clockPicker.val(), ["h:mm a", "H:MM"]);
+                        var modelTime = moment(scope.model);
+
+                        modelTime.hour(pickerTime.hours());
+                        modelTime.minute(pickerTime.minutes());
+
+                        if (scope.min && moment(scope.min).isAfter(modelTime)) {
+                            scope.model = min;
+                        } else {
+                            scope.model = modelTime.toISOString();
+                        }
+
+                        scope.$apply();
+                        onChange();
+                        scope.$apply();
+                    }
+                });
+
+                var dateInput = jQuery('.datepicker').pickadate({
+                    format: 'm/d/yyyy',
+                    formatSubmit: 'm/d/yyyy',
+                    onClose: function () {
+                        var pickerTime = moment(dateInput.val(), ['M/D/YY', 'D.M.YY']);
+                        var modelTime = moment(scope.model);
+
+                        modelTime.year(pickerTime.year());
+                        modelTime.month(pickerTime.month());
+                        modelTime.date(pickerTime.date());
+
+                        if (scope.min && moment(scope.min).isAfter(modelTime)) {
+                            scope.model = min;
+                        } else {
+                            scope.model = modelTime.toISOString();
+                        }
+
+                        scope.$apply();
+                        onChange();
+                        scope.$apply();
+                        clockPicker.clockpicker('show');
+                    }
+                });
+
+                var datePicker = dateInput.pickadate('picker');
+
+                scope
+                    .$watch(
+                        'model',
+                        function (model) {
+                            var time;
+
+                            if (model) {
+                                time = moment(model);
+                            } else {
+                                time = moment();
+                            }
+
+                            clockPicker.val(time.format('LT'))
+                            dateInput.val(time.format('l'));
+                            datePicker.set('select', time.toDate())
+                        });
+                scope
+                    .$watch(
+                        'min',
+                        function (min) {
+                            scope.min = min;
+                        });
+
+                function onChange() {
+                    if (scope.change) {
+                        scope.$parent.$eval(scope.change);
+                    }
+                }
+            }
+        }
     })
     .directive('tiCheckbox', function ($timeout, $parse) {
         return {
@@ -539,7 +677,35 @@ angular.module("ThingItMobile.PluginDirectives", [])
             jQuery(element).append(i);
         }
     };
-}).directive('tiMotionSensor', function ($timeout, $parse) {
+}).directive('tiClickWait', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function link(scope, element, attr) {
+            var fn = $parse(attr['tiClickWait']);
+            var target = element[0];
+            var content = element.html();
+
+            element.on('click', function (event) {
+                safeApply(scope, function () {
+                    attr.$set('disabled', true);
+                    element.html('<i class="fa fa-circle-o-notch fa-spin"></i> ' + content);
+                    //element.width(oldWidth + oldWidth / 2); // make room for spinner
+
+                    fn(scope, {$event: event})
+                        .done(function (res) {
+                            element.html(content);
+                            //element.width(oldWidth); // restore size
+                            attr.$set('disabled', false);
+                            return res;
+                        }).fail(function (res) {
+                        element.html(content);
+                        attr.$set('disabled', false);
+                    });
+                });
+            });
+        }
+    };
+}]).directive('tiMotionSensor', function ($timeout, $parse) {
     return {
         restrict: "E",
         template: "<div style='display: table-row; height: 2em; vertical-align: middle'><div style='display: table-cell; width: 100%; text-align: center'><i class='icon sl-contacts-1 overlay-alarm-icon' style='display: inline-block:'></i><span class='smallFont lightPrimaryColor'>No Motion detected.</span></div></div>",
@@ -597,8 +763,311 @@ angular.module("ThingItMobile.PluginDirectives", [])
             });
         }
     };
-});
+}).directive("tiSvg", function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        template: '<div class="canvas"><div ng-transclude></div></div>',
+        controller: function ($scope) {
+            $scope.element = [];
 
+            this.addElement = function (element) {
+                $scope.element.push(element);
+            }
+        },
+        link: function (scope, element, attrs) {
+            var canvas = jQuery(element);
+            var id = 'ti-svg-' + Utils.getUuid()
+
+            canvas.prop('id', id);
+            canvas.css('max-height', 400);
+
+            scope.paper = Raphael(id);
+
+            scope.paper.setViewBox(0, 0, 600, 400, true);
+            scope.paper.setSize('100%', '100%');
+
+            for (var n in scope.element) {
+                if (scope.element[n].type == 'rect') {
+                    scope.element[n].svg = scope.paper.rect(0, 0, 0, 0, 5);
+                } else if (scope.element[n].type == 'text') {
+                    scope.element[n].svg = scope.paper.text(0, 0, '');
+                } else if (scope.element[n].type == 'path') {
+                    scope.element[n].svg = scope.paper.path('M0 0');
+                } else if (scope.element[n].type == 'ellipse') {
+                    scope.element[n].svg = scope.paper.ellipse(0, 0, 0, 0);
+                }
+            }
+        }
+    };
+}).directive("tiSvgRect", function () {
+    return {
+        restrict: 'E',
+        require: '^tiSvg',
+        replace: true,
+        template: '',
+        link: function (scope, element, attrs, outerController) {
+            var rect = {type: 'rect'};
+
+            outerController.addElement(rect);
+
+            scope
+                .$watch(
+                    attrs.tiAttrs,
+                    function (value) {
+                        if (value && rect.svg) {
+                            rect.svg.attr(value);
+                        }
+                    }, true);
+        }
+    };
+}).directive("tiSvgText", function () {
+    return {
+        restrict: 'E',
+        require: '^tiSvg',
+        replace: true,
+        template: '',
+        link: function (scope, element, attrs, outerController) {
+            var text = {type: 'text'};
+
+            outerController.addElement(text);
+
+            scope
+                .$watch(
+                    attrs.tiAttrs,
+                    function (value) {
+                        if (value && text.svg) {
+                            text.svg.attr(value);
+                        }
+                    }, true);
+        }
+    };
+}).directive("tiSvgPath", function () {
+    return {
+        restrict: 'E',
+        require: '^tiSvg',
+        replace: true,
+        template: '',
+        link: function (scope, element, attrs, outerController) {
+            var path = {type: 'path'};
+
+            outerController.addElement(path);
+
+            scope
+                .$watch(
+                    attrs.tiAttrs,
+                    function (value) {
+                        if (value && path.svg) {
+                            path.svg.attr(value);
+                        }
+                    }, true);
+        }
+    };
+}).directive("tiSvgEllipse", function () {
+    return {
+        restrict: 'E',
+        require: '^tiSvg',
+        replace: true,
+        template: '',
+        link: function (scope, element, attrs, outerController) {
+            var ellipse = {type: 'ellipse'};
+
+            outerController.addElement(ellipse);
+
+            scope
+                .$watch(
+                    attrs.tiAttrs,
+                    function (value) {
+                        if (value && ellipse.svg) {
+                            ellipse.svg.attr(value);
+                        }
+                    }, true);
+        }
+    };
+}).directive(
+    'tiMonthSchedule',
+    function ($log, $filter) {
+        return {
+            restrict: 'E',
+            require: 'ngModel',
+            scope: {
+                model: "=ngModel",
+                month: "=tiMonth",
+                dayClick: "@tiDayClick"
+            },
+            templateUrl: 'calendar.html',
+            controller: ["$scope", function ($scope) {
+                $scope.now = moment();
+
+                $scope
+                    .$watch(
+                        'month',
+                        function (month) {
+                            if (month) {
+                                $scope.array = [];
+
+                                month.startOf('month');
+
+                                for (var n = month.day(); n > 0; --n) {
+                                    $scope.array.push(month.clone().subtract(n, 'days'));
+                                }
+
+                                $scope.array.push(month.clone());
+
+                                for (var n = 1; $scope.array.length < 35; ++n) {
+                                    $scope.array.push(month.clone().add(n, 'days'));
+                                }
+
+                                $scope.$apply();
+                            }
+                        }, true);
+
+                this.dayClick = function (day) {
+                    if ($scope.dayClick) {
+                        var method = $scope.dayClick.substring(0, $scope.dayClick.lastIndexOf('('));
+                        var object = $scope.dayClick.substring(0, $scope.dayClick.lastIndexOf('.'));
+                        var call = $scope.$parent.$eval(method).bind($scope.$parent.$eval(object));
+
+                        call(day);
+                    }
+                };
+                this.getDayClasses = function (date) {
+                    var classes = 'day';
+                    var now = moment();
+
+                    if (date && (date.date() == now.date() && date.month() == now.month() && date.year() == now.year())) {
+                        classes += ' today';
+                    } else {
+                        if (date.month() !== $scope.month.month()) {
+                            classes += ' otherMonth';
+                        } else {
+                            if (date.day() == 0 || date.day() == 6) {
+                                classes += ' holiday';
+                            }
+                        }
+
+                        if (date.isBefore(now)) {
+                            classes += ' pastDate';
+                        }
+                    }
+
+                    return classes;
+                };
+            }],
+            controllerAs: "calendar"
+        }
+    }).directive(
+    'tiDaySchedule',
+    function ($log, $filter) {
+        return {
+            restrict: 'E',
+            require: 'ngModel',
+            scope: {
+                model: "=ngModel",
+                day: "=tiDay",
+                eventClick: "@tiEventClick",
+                click: "@ngClick"
+            },
+            templateUrl: 'day.html',
+            controller: ["$scope", function ($scope) {
+                $scope.now = moment();
+
+                $scope
+                    .$watch(
+                        'day',
+                        function (day) {
+                            if (day) {
+                                $scope.hours = [];
+
+                                day.startOf('day');
+
+                                $scope.hours.push(day.clone());
+
+                                for (var n = 1; n <= 24; ++n) {
+                                    $scope.hours.push(day.clone().add(n, 'hours'));
+                                }
+
+                                $scope.$apply();
+                            }
+                        }, true);
+
+                this.moment = function (timestamp) {
+                    return moment(timestamp);
+                };
+                this.differenceInMinutes = function (timestamp1, timestamp2) {
+                    return Math.abs(moment.duration(timestamp1.diff(timestamp2)).asMinutes());
+                };
+                this.differenceInHours = function (event) {
+                    return Math.abs(moment.duration(event.end.diff(event.start)).asHours());
+                };
+                this.isInHour = function (timestamp, hour) {
+                    return hour.isSameOrBefore(timestamp) && hour.clone().add(1, 'hours').isAfter(timestamp);
+                };
+                this.eventClick = function (event) {
+                    if ($scope.eventClick) {
+                        var method = $scope.eventClick.substring(0, $scope.eventClick.lastIndexOf('('));
+                        var object = $scope.eventClick.substring(0, $scope.eventClick.lastIndexOf('.'));
+                        var call = $scope.$parent.$eval(method).bind($scope.$parent.$eval(object));
+
+                        call(event);
+                    }
+                };
+                this.click = function (hour) {
+                    if ($scope.click) {
+                        var method = $scope.click.substring(0, $scope.click.lastIndexOf('('));
+                        var object = $scope.click.substring(0, $scope.click.lastIndexOf('.'));
+                        var call = $scope.$parent.$eval(method).bind($scope.$parent.$eval(object));
+
+                        call(hour);
+                    }
+                };
+            }],
+            controllerAs: "daySchedule"
+        }
+    })
+// TODO Move to ui directive
+    .directive(
+        'tiMarkup',
+        function ($timeout, $parse) {
+            return {
+                link: function (scope,
+                                element,
+                                attrs) {
+                    attrs
+                        .$observe(
+                            'ngModel',
+                            function (path) {
+                                scope
+                                    .$watch(
+                                        path,
+                                        function (value) {
+                                            if (value) {
+                                                jQuery(
+                                                    element)
+                                                    .empty();
+                                                jQuery(
+                                                    element)
+                                                    .append(
+                                                        marked(value));
+                                            }
+                                        });
+                            });
+                }
+            };
+        })
+;
+
+function safeApply(scope, fn) {
+    var phase = scope.$root.$$phase;
+    if (phase == '$apply' || phase == '$digest') {
+        if (fn && (typeof(fn) === 'function')) {
+            fn();
+        }
+    } else {
+        scope.$apply(fn);
+    }
+};
 
 /**
  * Does not write to the model! Could do if we would block watches during change.
