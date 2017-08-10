a909502963013a27b3e2178acd3fa87cbc1a5c78
marcgille <latakia42>
2017-08-10 14:31:16 +0200

    Event processor fixes and parameter passing.

--- bin/thing-it-peripheral.js
+++
@@ -1,5 +0,0 @@
-var bleno = require('bleno');
-var util = require('util');
-var process = require('process');
-var fs = require('fs');
-
--- test/testConfigurationManager.js
+++
@@ -1,5 +0,0 @@
-var ConfigurationManager = require("../lib/configurationManager");
-
-var configurationManager = ConfigurationManager.create(process.cwd() + '/tmp', process.cwd() + '/tmp');
-
-configurationManager.listen();
